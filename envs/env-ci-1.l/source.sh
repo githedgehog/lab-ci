86f10baca628f72e7802e1f492506f240b27bfbd
Sergei Lukianov <me@slukjanov.name>
2025-11-05 12:32:50 -0800

    chore: extend with gw support
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- envs/env-ci-1.l/source.sh
+++ envs/env-ci-1.l/source.sh
@@ -2,4 +2,6 @@
 
 export HHFAB_TLS_SAN=env-ci-1.l.hhdev.io
 export HHFAB_CONTROL_NODE_MGMT_LINK="pci@0000:01:00.3"
+export HHFAB_GATEWAY_NODE_MGMT_LINK="pci@0000:01:00.2"
 export HHFAB_FABRIC_MODE=spine-leaf
+export HHFAB_GATEWAY=true
