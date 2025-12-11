89f914841b5dc3711c3ec6a155a78108adc184c4
Sergei Lukianov <me@slukjanov.name>
2025-12-11 13:01:01 -0800

    chore: prep for multigw and new node mgmt links env config
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- envs/env-ci-1.l/source.sh
+++ envs/env-ci-1.l/source.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 export HHFAB_TLS_SAN=env-ci-1.l.hhdev.io
+export HHFAB_FABRIC_MODE=spine-leaf
+
+# pre-25.05
 export HHFAB_CONTROL_NODE_MGMT_LINK="pci@0000:01:00.3"
 export HHFAB_GATEWAY_NODE_MGMT_LINK="pci@0000:01:00.2"
-export HHFAB_FABRIC_MODE=spine-leaf
+
+# 25.06+
+export HHFAB_NODE_MGMT_LINKS="control-1=pci@0000:01:00.3,gateway-1=pci@0000:01:00.2"
