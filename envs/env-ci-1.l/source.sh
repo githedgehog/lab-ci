e159a4289f9204ec44a5775c0b51f23c60c1f2cf
Steven Noble <snoble@sonn.com>
2026-03-30 21:54:57 +0000

    chore: update pci mappings for new r750 server
    Signed-off-by: Steven Noble <snoble@sonn.com>

--- envs/env-ci-1.l/source.sh
+++ envs/env-ci-1.l/source.sh
@@ -6,4 +6,4 @@ export HHFAB_FABRIC_MODE=spine-leaf
 export HHFAB_VLAB_SRV_CPUS=4
 export HHFAB_VLAB_SRV_RAM=2048
 
-export HHFAB_OOB_MGMT_IFACE=eno4 # pci@0000:01:00.3, formerly control-1 mgmt passthrough
+export HHFAB_OOB_MGMT_IFACE=eno12399 # pci@0000:01:00.3, formerly control-1 mgmt passthrough
